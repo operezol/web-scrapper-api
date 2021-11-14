f2b203da613e0bf13ad0c0ab9890cdfa5ebe27d0
operezol <oriolperezolivares@gmail.com>
2021-11-14 13:55:50 +0100

    fix heroku pupeteer

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const getData = async (url, DOMQuery) => {
     try {
         const browser = await puppeteer.launch({
             headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--single-process'
-            ],
-        });
+            args: ['--no-sandbox','--disable-setuid-sandbox']
+          })
         const page = await browser.newPage();
         await page.goto(url, {
             waitUntil: 'domcontentloaded',
