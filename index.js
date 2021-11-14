e97a6ab679f76224de2e6fc0e72112b2cef18960
operezol <oriolperezolivares@gmail.com>
2021-11-14 12:49:47 +0100

    formated index

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const PORT = 8000
-const express = require('express')
-const puppeteer = require("puppeteer")
+const PORT = process.env.port || 8000;
+const express = require('express');
+const puppeteer = require("puppeteer");
 
-const app = express()
+const app = express();
 
 const getData = async (url, DOMQuery) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     await page.goto(url, {
         waitUntil: 'domcontentloaded',
         timeout: 0
-    })
+    });
     const urls = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("a")).map(x => x.href)
-    })
+        return Array.from(document.querySelectorAll("a")).map(x => x.href);
+    });
     const DOMQueryResult = await page.evaluate((DOMQuery) => {
-        return Array.from(document.querySelectorAll(DOMQuery)).map(el=>el.textContent)
-    }, DOMQuery)
-    await browser.close()
-    return { urls, DOMQueryResult }
-}
+        return Array.from(document.querySelectorAll(DOMQuery)).map(el => el.textContent);
+    }, DOMQuery);
+    await browser.close();
+    return { urls, DOMQueryResult };
+};
 
-app.get('/',async(request,response)=>{
-    const {url, DOMQuery} = request.query;
-    const data = await getData(url,DOMQuery)
-    response.json(data)
-})
+app.get('/', async (request, response) => {
+    const { url, DOMQuery } = request.query;
+    const data = await getData(url, DOMQuery);
+    response.json(data);
+});
 
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
