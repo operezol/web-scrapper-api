84643b5bf2be2af549235787ddbce753d0f29c0b
operezol <oriolperezolivares@gmail.com>
2021-11-14 13:35:45 +0100

    try to fix heroku timeout

--- index.js
+++ index.js
@@ -2,28 +2,34 @@ const express = require('express');
 const puppeteer = require("puppeteer");
 const app = express();
 const getData = async (url, DOMQuery) => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--single-process'
-        ],
-    });
-    const page = await browser.newPage();
-    await page.goto(url, {
-        waitUntil: 'domcontentloaded',
-        timeout: 0
-    });
-    const urls = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("a")).map(x => x.href);
-    });
-    const DOMQueryResult = await page.evaluate((DOMQuery) => {
-        return Array.from(document.querySelectorAll(DOMQuery)).map(el => el.textContent);
-    }, DOMQuery);
-    await browser.close();
-    return { urls, DOMQueryResult };
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--single-process'
+            ],
+        });
+        const page = await browser.newPage();
+        await page.goto(url, {
+            waitUntil: 'domcontentloaded',
+            timeout: 0
+        });
+        const urls = await page.evaluate(() => {
+            return Array.from(document.querySelectorAll("a")).map(x => x.href);
+        });
+        const DOMQueryResult = await page.evaluate((DOMQuery) => {
+            return Array.from(document.querySelectorAll(DOMQuery)).map(el => el.textContent);
+        }, DOMQuery);
+        await browser.close();
+        return { urls, DOMQueryResult };
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+
 };
 app.get('/', async (request, response) => {
     const { url, DOMQuery } = request.query;
